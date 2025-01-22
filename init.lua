15821826474becd80bd4102327c9965fc86dae4e
Akash <yousuckatcoding@proton.me>
2025-01-22 21:25:19 +0530

    Add maps to ensure that we use ctrl + vim keybindings for pane navigation

--- init.lua
+++ init.lua
@@ -7,3 +7,12 @@ vim.api.nvim_set_option("clipboard","unnamed")
 vim.cmd.colorscheme("nordic")
 
 vim.cmd("Copilot disable")
+
+-- Move to the left pane
+vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+-- Move to the right pane
+vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+-- Move to the upper pane
+vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+-- Move to the lower pane
+vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
