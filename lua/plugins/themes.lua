238d547d152ce8f0c44e07f99da0c590bf8cdf3c
akash <akash.sivanandan@pricelabs.co>
2025-01-22 20:11:19 +0530

    Add more themes to nvim

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,3 +1,23 @@
 return {
-	"rebelot/kanagawa.nvim"
+  {
+    "rebelot/kanagawa.nvim",
+    version = "*"
+  },
+  {
+    "tiagovla/tokyodark.nvim",
+    version = "*"
+  },
+  {
+    "Mofiqul/dracula.nvim",
+    version = "*"
+
+  },
+  {
+    'AlexvZyl/nordic.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require('nordic').load()
+    end
+  }
 }
