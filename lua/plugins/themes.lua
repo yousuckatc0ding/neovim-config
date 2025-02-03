fa79f45da7a34ac87c9e7a4352096b95f9aeb900
Akash <yousuckatcoding@proton.me>
2025-02-03 16:56:36 +0530

    Add more themes

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -16,8 +16,21 @@ return {
     'AlexvZyl/nordic.nvim',
     lazy = false,
     priority = 1000,
-    config = function()
-        require('nordic').load()
-    end
-  }
+    -- config = function()
+    --     require('nordic').load()
+    -- end
+  },
+  {
+	  "embark-theme/vim",
+	  as = "embark",
+	  config = function()
+		  vim.cmd.colorscheme("embark")
+	  end
+  },
+  {
+    'bettervim/yugen.nvim',
+    -- config = function()
+    --     vim.cmd.colorscheme('yugen')
+    -- end,
+  },
 }
